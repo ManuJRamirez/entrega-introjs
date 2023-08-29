1f6a794d71d7b8c8ad9256521196dd0139607d54
Manuel Ramirez <manramloz@gmail.com>
2023-08-29 20:53:10 +0200

    terminado el partido

--- wimblecode.js
+++ wimblecode.js
@@ -27,10 +27,10 @@ const createMatch = (j1, j2) => {
             reinicioPuntuacion();
         }else if (jugador2.puntuacion >= 4 && jugador2.puntuacion - jugador1.puntuacion >= 2){
             jugador2.rondasGanadas += 1;
-            reinicioPuntuacion();
-            
+            reinicioPuntuacion();        
         }
-    }
+    };
+    
     const getCurrentRoundScore = () =>{
         if(jugador1.puntuacion >= 3 && jugador2.puntuacion >= 3){
             if(jugador1.puntuacion === jugador2.puntuacion){
@@ -43,18 +43,52 @@ const createMatch = (j1, j2) => {
         }else {
             return (`${jugador1.nombre} ${puntuaciones[jugador1.puntuacion]}-${puntuaciones[jugador2.puntuacion]} ${jugador2.nombre}`);
         }
-    }
+    };
+    
     const getGameScore = () => {
         return (`${jugador1.nombre} ${jugador1.rondasGanadas}\n${jugador2.nombre} ${jugador2.rondasGanadas}`);
+    };
+
+    const getMatchScore = () => {
+        if(jugador1.rondasGanadas <= 7 && jugador2.rondasGanadas <= 7){
+            if(jugador1.rondasGanadas === 7){
+                jugador1.juegosGanados += 1;
+                reinicioRondas();
+            }else if (jugador1.rondasGanadas >= 4 && jugador1.rondasGanadas - jugador2.rondasGanadas >= 2){
+                jugador1.juegosGanados += 1;
+                reinicioRondas();
+            }else if (jugador2.rondasGanadas === 7){
+                jugador2.juegosGanados += 1;
+                reinicioRondas();
+            }else if (jugador2.rondasGanadas >= 4 && jugador2.rondasGanadas - jugador1.rondasGanadas >= 2){
+                jugador2.juegosGanados += 1;
+                reinicioRondas();
+            return (`${jugador1.nombre} ${jugador1.juegosGanados}\n${jugador2.nombre} ${jugador2.juegosGanados}`);
+            }
+        }
+    };
+
+    const getWinner = () => {
+        if(jugador1.juegosGanados === 2){
+            return jugador1.nombre;
+        }
+        else if(jugador2.juegosGanados === 2){
+            return jugador2.nombre;
+        }
     }
+    
 
     const reinicioPuntuacion = () => {
         jugador1.puntuacion = 0;
         jugador2.puntuacion = 0;
-    }
+    };
     const reinicioRondas = () => {
         jugador1.rondasGanadas = 0;
         jugador2.rondasGanadas = 0;
+    };
+    const reinicioJuegos = () => {
+        jugador1.juegosGanados = 0;
+        jugador2.juegosGanados = 0;
     }
 
     
@@ -62,8 +96,11 @@ const createMatch = (j1, j2) => {
         pointWonBy,
         getCurrentRoundScore,
         getGameScore,
+        getMatchScore,
+        getWinner,
     };
 }
+
 
 const game = createMatch('Alberto C', 'David J');
 // Cuando puntua el 1º judagor lo registro de este modo
@@ -96,17 +133,38 @@ game.pointWonBy(2); // Player 2 wins the game
 game.pointWonBy(2); // Player 2 wins the game
 game.pointWonBy(2); // Player 2 wins the game
 // David gana 2º ronda
-console.log(game.getGameScore());
 game.pointWonBy(2); // Player 2 wins the game
 game.pointWonBy(2); // Player 2 wins the game
 game.pointWonBy(2); // Player 2 wins the game
 game.pointWonBy(2); // Player 2 wins the game
 // David gana 3º ronda
-console.log(game.getGameScore());
 game.pointWonBy(2); // Player 2 wins the game
 game.pointWonBy(2); // Player 2 wins the game
 game.pointWonBy(2); // Player 2 wins the game
 game.pointWonBy(2); // Player 2 wins the game
-console.log(game.getGameScore());
 // David gana 4º ronda
 // Primer juego ganado
+console.log(game.getMatchScore()); // Output: "Alberto C 0\nDavid J 1"
+game.pointWonBy(2); // Player 2 wins the game
+game.pointWonBy(2); // Player 2 wins the game
+game.pointWonBy(2); // Player 2 wins the game
+game.pointWonBy(2); // Player 2 wins the game
+// gana ronda 1º
+game.pointWonBy(2); // Player 2 wins the game
+game.pointWonBy(2); // Player 2 wins the game
+game.pointWonBy(2); // Player 2 wins the game
+game.pointWonBy(2); // Player 2 wins the game
+// gana ronda 2º
+game.pointWonBy(2); // Player 2 wins the game
+game.pointWonBy(2); // Player 2 wins the game
+game.pointWonBy(2); // Player 2 wins the game
+game.pointWonBy(2); // Player 2 wins the game
+// gana ronda 3º
+game.pointWonBy(2); // Player 2 wins the game
+game.pointWonBy(2); // Player 2 wins the game
+game.pointWonBy(2); // Player 2 wins the game
+game.pointWonBy(2); // Player 2 wins the game
+// gana ronda 4º
+// Método para ver los juegos de cada jugador
+console.log(game.getMatchScore()); // Output: "Alberto C 0\nDavid J 2";
+console.log(game.getWinner()); // Output: "David J"
